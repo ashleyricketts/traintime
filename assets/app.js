4ab9bf3d402ce82860b4cbf5bd9513495a317141
Ashley Ricketts <aricketts12@gmail.com>
2019-03-14 01:50:39 -0500

    function changes for append to table

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@ var config = {
   };
 
   firebase.initializeApp(config);
-
+      
   var database = firebase.database();
 
   
@@ -29,6 +29,54 @@ var config = {
 
   currentTime();
 
-  
+  // values from form 
+  $("#add-train-btn").on("click", function(event){
+          event.preventDefault();
 
-        
+          var trainName = $("#train-name-input").val().trim();
+          var destination = $("#destination-input").val().trim();
+          var startTime = $("#time-input").val().trim();
+          var frequency = $("#frequency-input").val().trim();
+
+          database.ref().push({
+                  trainName: trainName,
+                  destination: destination,
+                  startTime: startTime,
+                  frequency: frequency,
+          })
+
+          //clear form
+          $("#train-name-input").val("");
+          $("#destination-input").val("");
+          $("#time-input").val("");
+          $("#frequency-input").val("");
+  });
+
+
+  // insert form values to table 
+  
+  database.ref().on("child_added", function(childSnapshot) {
+        var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+        var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+        var timeRemain = timeDiff % childSnapshot.val().frequency;
+        var minToArrival = childSnapshot.val().frequency - timeRemain;
+        var nextTrain = moment().add(minToArrival, "minutes");
+        var key = childSnapshot.key;
+  
+    var newrow = $("<tr>");
+        newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
+        newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
+        newrow.append($("<td class='text-center'>" + childSnapshot.val().frequency + "</td>"));
+        newrow.append($("<td class='text-center'>" + moment(nextTrain).format("LT") + "</td>"));
+        newrow.append($("<td class='text-center'>" + minToArrival + "</td>"));
+  
+    if (minToArrival < 6) {
+      newrow.addClass("info");
+    }
+  
+    $("#train-table-rows").append(newrow);
+  
+  });
+
+
+ 
